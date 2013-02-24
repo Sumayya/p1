93aff25ef3691609ee2b5b80a248e9cee77348dd
Sumayya <tabassumsumayya@rocketmail.com>
2013-02-24 10:01:47 -0800

    Iam Commiting

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+P1::Application.config.secret_token = 'b88ed5a26c52066b72c55b5b8cf2a1eea229144605b4d923ff6fcaec4eecdc7bc4b1dfb73f8f28737c1d121676d0b9d1f26323b51dc59f3716f33fe7b2d1cfeb'
